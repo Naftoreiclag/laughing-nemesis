cac60aaf9ff783808c599c303c2e994b6df9410c
Naftoreiclag <naftoreiclag@gmail.com>
2014-10-07 21:34:58 -0700

    Cooking by the book

--- source/naftoreiclag/laughingnemesis/Person.java
+++ source/naftoreiclag/laughingnemesis/Person.java
@@ -1,30 +1,23 @@
 package naftoreiclag.laughingnemesis;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import com.sun.jmx.snmp.tasks.Task;
-
 import naftoreiclag.laughingnemesis.communication.Bubble;
-import naftoreiclag.pigcollision.Circle;
 
 // Goal is to make as many friends as possible.
 public class Person implements ITickable
 {
 	public Gender gender;
 	public String name;
-	
 	public Body body;
 	
 	public Stamina brainStamina = new Stamina();
 	
-	public Colour favColor;
-	
-	public List<Foo> recentSayings = new LinkedList<Foo>();
-	
-	private MemoryCollection memories = new MemoryCollection();
+	public MemoryCollection memories = new MemoryCollection();
+	public List<Task> queuedTasks = new LinkedList<Task>();
+	public Task currentTask;
 	
 	public Person(World world)
 	{
@@ -32,43 +25,30 @@ public class Person implements ITickable
 		
 		gender = Gender.getRandomGender();
 		name = Names.getRandomName(gender);
-		
-		favColor = Colour.getRandomColor();
-
-	}
-	
-	private void saySomething(String something)
-	{
-		recentSayings.add(new Foo(5, something));
 	}
 
 	@Override
 	public void tick(double delta)
 	{
-		// Tick all sub-tickables
 		memories.tick(delta);
 		body.tick(delta);
 		brainStamina.tick(delta);
-		
-		
-		for(Foo foo : recentSayings)
-		{
-			foo.age -= delta;
-		}
-		
-		if(GR.chanceOverTime(delta, 0.5))
-		{
-			this.saySomething("yo");
-		}
-		
-		
-		if(!bubbles.isEmpty())
-		{
-			Bubble bubble = bubbles.remove();
-		}
-		
+	}
+
+	
+	
+	
+	
+	
+
+	
+	private void saySomething(String something)
+	{
+		recentSayings.add(new Foo(5, something));
 	}
 	
+	
+	public List<Foo> recentSayings = new LinkedList<Foo>();
 	public Queue<Bubble> bubbles = new LinkedList<Bubble>();
 	
 	public void receiveMessage(Bubble bubble)
